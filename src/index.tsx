3cbf5fd88638585f80acf92a5479628048f55c78
PrateekUpadhyay63 <pratee.upadhyay@mail.vinove.com>
2024-05-10 17:28:15 +0530

    First Commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export function Comply() {
+  return (
+    <div>
+      <div style={{ height: '100vh', overflow: 'hidden' }}>
+        <embed src={'http://122.176.139.248:8092/'} width={'100%'} height={'100%'} />
+      </div>
+    </div>
+  )
+}
